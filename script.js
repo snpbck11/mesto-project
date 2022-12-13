83e3f49b18600f4a799082c700e226b9ace5b1eb
Boris <savelyevbb@mail.ru>
2022-12-13 12:08:35 +0500

    add cards addFunction

--- script.js
+++ script.js
@@ -1,33 +1,82 @@
-const popup = document.querySelector('.popup');
-const formElement = document.querySelector('.form');
-const nameInput = formElement.querySelector('#person-name');
-const jobInput = formElement.querySelector('#person-about');
+const popupEdit = document.querySelector('.popup-edit');  
+const popupAdd = document.querySelector('.popup-add');
+const formEdit = document.querySelector('.form-edit');
+const formAdd = document.querySelector('.form-add');
+const nameInput = formEdit.querySelector('#person-name'); 
+const aboutInput = formEdit.querySelector('#person-about');
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
+const addButton = document.querySelector('.profile__add-button');
+const gallery = document.querySelector('.gallery');
 
-// Обработчик «отправки» формы, хотя пока
+// Обработчик «отправки» формы редактирования
 // она никуда отправляться не будет
-function handleFormSubmit(evt) {
+function editFormSubmit(evt) {
     evt.preventDefault();
     const name = nameInput.value;
-    const job = jobInput.value;  
+    const about = aboutInput.value;  
     document.querySelector('.profile__name').textContent = name;
-    document.querySelector('.profile__about').textContent = job;
-    toggleForm();
-}
-
-// Кнопка сабмита формы
-formElement.addEventListener('submit', handleFormSubmit);
-
-// Функия открытия и закрытия попапа
-function toggleForm() {
-  popup.classList.toggle('popup_opened');
+    document.querySelector('.profile__about').textContent = about;
+    popupEdit.classList.remove('popup_opened');
 };
 
+// Кнопка сабмита формы редактирования
+formEdit.addEventListener('submit', editFormSubmit);
 
-// Надо сделать функия открытия и закрытия попапа
+// Функция добавления карточек
+function addCard(titleValue, photoValue) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-editButton.addEventListener('click', toggleForm);
+  cardElement.querySelector('.card__title').textContent = titleValue;
+  cardElement.querySelector('.card__photo').src = photoValue;
+  cardElement.querySelector('.card__photo').alt = titleValue;
+  cardElement.querySelector('.card__like-button').addEventListener('click', function(evt) {
+     evt.target.classList.toggle('card__like-button_active');
+  });
+  
+  gallery.prepend(cardElement);  
+}
 
-closeButton.addEventListener('click', toggleForm);
+// Обработчик отправки формы добавления карточек
+function addFormSubmit(evt) {
+  evt.preventDefault();  
+  const pictureNameInput = formAdd.querySelector('#picture-name');
+  const urlInput = formAdd.querySelector('#picture-url');
+
+  addCard(pictureNameInput.value, urlInput.value);
+
+  popupAdd.classList.remove('popup_opened');
+
+  pictureNameInput.value = '';
+  urlInput.value = '';
+};
+
+// Кнопка сабмита формы добавления карточек
+formAdd.addEventListener('submit', addFormSubmit);
+
+// Функция открытия и закрытия попапов
+function popupHandler(trigger, popup,) {
+  trigger.addEventListener('click', () => {
+    popup.classList.add('popup_opened');
+  });
+  const close = popup.querySelector('.popup__close-button');
+  close.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
+  });
+}
+// Вызов функции окна редактирования
+popupHandler(editButton, popupEdit);
+
+// Вызов функции окна добавления карточек
+popupHandler(addButton, popupAdd);
+
+
+
+
+
+
+
+
+
+
 
