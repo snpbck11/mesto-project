616a7c5c66207f57996742934420805026e8af61
Boris <savelyevbb@mail.ru>
2022-12-13 18:45:10 +0500

    added func view cards

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const popupEdit = document.querySelector('.popup-edit');  
 const popupAdd = document.querySelector('.popup-add');
+const popupPicture = document.querySelector('.popup-open-picture');
 const formEdit = document.querySelector('.form-edit');
 const formAdd = document.querySelector('.form-add');
 const nameInput = formEdit.querySelector('#person-name'); 
@@ -7,6 +8,30 @@ const aboutInput = formEdit.querySelector('#person-about');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const gallery = document.querySelector('.gallery');
+
+// Функция открытия и закрытия попапов (попробовать реализовать закрытие нажатием на оверлей)
+function popupHandler(trigger, popup,) {
+  trigger.addEventListener('click', () => {
+    popup.classList.add('popup_opened');
+  });
+  const close = popup.querySelector('.popup__close-button');
+  close.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
+  });
+  if (popup.className.includes('overlay')) {
+    popup.addEventListener('mousedown', function(evt) {
+      if (evt.target === popup) {
+        popup.classList.remove('popup_opened');
+      };
+    });
+  };
+};
+// Вызов функции окна редактирования
+popupHandler(editButton, popupEdit);
+
+// Вызов функции окна добавления карточек
+popupHandler(addButton, popupAdd);
+
 
 // Обработчик «отправки» формы редактирования
 // она никуда отправляться не будет
@@ -22,7 +47,7 @@ function editFormSubmit(evt) {
 // Кнопка сабмита формы редактирования
 formEdit.addEventListener('submit', editFormSubmit);
 
-// Функция добавления карточек
+// Функция добавления, удаления и просмотра карточек
 function addCard(titleValue, photoValue) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
@@ -30,14 +55,23 @@ function addCard(titleValue, photoValue) {
   cardElement.querySelector('.card__title').textContent = titleValue;
   cardElement.querySelector('.card__photo').src = photoValue;
   cardElement.querySelector('.card__photo').alt = titleValue;
+  cardElement.querySelector('.card__photo').addEventListener('click', function(evt) {
+    document.querySelector('.popup-open-picture__image').src = photoValue;
+    document.querySelector('.popup-open-picture__image').alt = photoValue;
+    document.querySelector('.popup-open-picture__caption').textContent = titleValue;
+    popupHandler(evt.target, popupPicture);
+  });
   cardElement.querySelector('.card__like-button').addEventListener('click', function(evt) {
      evt.target.classList.toggle('card__like-button_active');
   });
+  cardElement.querySelector('.card__trash-button').addEventListener('click', () => {
+    cardElement.remove();
+  });
   
   gallery.prepend(cardElement);  
-}
+};
 
-// Обработчик отправки формы добавления карточек
+// Обработчик формы добавления карточек
 function addFormSubmit(evt) {
   evt.preventDefault();  
   const pictureNameInput = formAdd.querySelector('#picture-name');
@@ -54,21 +88,24 @@ function addFormSubmit(evt) {
 // Кнопка сабмита формы добавления карточек
 formAdd.addEventListener('submit', addFormSubmit);
 
-// Функция открытия и закрытия попапов
-function popupHandler(trigger, popup,) {
-  trigger.addEventListener('click', () => {
-    popup.classList.add('popup_opened');
-  });
-  const close = popup.querySelector('.popup__close-button');
-  close.addEventListener('click', () => {
-    popup.classList.remove('popup_opened');
-  });
-}
-// Вызов функции окна редактирования
-popupHandler(editButton, popupEdit);
 
-// Вызов функции окна добавления карточек
-popupHandler(addButton, popupAdd);
+
+
+// Функция заполнения галереи карточками из массива
+function addCardsArray (array) {
+  array.forEach(element => {
+    const name = element.name;
+    const link = element.link;
+    addCard(name, link);    
+  });
+};
+
+// Вызов функции заполнения галереи 
+addCardsArray(initialCards);
+
+// Функция открытия картинки
+
+
 
 
 
