5bde3926927870c8377d6c524e740ce584938b90
Boris <savelyevbb@mail.ru>
2023-02-20 09:49:16 +0500

    validate v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
 const popupEdit = document.querySelector('.popup-edit');  
 const popupAdd = document.querySelector('.popup-add');
-const popupPicture = document.querySelector('.popup-open-picture');
-const popupPictureClose = popupPicture.querySelector('.popup__close-button');
-const formEdit = document.querySelector('.form-edit');
-const formAdd = document.querySelector('.form-add');
+const popupPicture = document.querySelector('.popup-picture');
+// Формы и их содержимое
+const formEdit = document.forms.edit;
+const formAdd = document.forms.add;
+// Элементы формы Edit
+const nameInput = formEdit.elements.name;
+const aboutInput = formEdit.elements.about;
+// Элементы формы Add
+const pictureNameInput = formAdd.elements.title;
+const linkInput = formAdd.elements.link;
+// 
 const profileName = document.querySelector('.profile__name');
-const profileAbout = document.querySelector('.profile__about');
-const nameInput = formEdit.querySelector('#person-name'); 
-const aboutInput = formEdit.querySelector('#person-about');
-const pictureNameInput = formAdd.querySelector('#picture-name');
-const linkInput = formAdd.querySelector('#picture-link');
+const profileAbout = document.querySelector('.profile__about');;
 const profileEditButton = document.querySelector('.profile__edit-button');
 const cardsAddButton = document.querySelector('.profile__add-button');
 const gallery = document.querySelector('.gallery');
@@ -50,8 +53,7 @@ popupHandler(profileEditButton, popupEdit, () => {
 
 // Вызов функции попапа с формой добавления карточек
 popupHandler(cardsAddButton, popupAdd, () => {
-  pictureNameInput.value = '';
-  linkInput.value = ''
+  formAdd.reset();
 });
 
 // Обработчик «отправки» формы редактирования
@@ -63,7 +65,7 @@ function editFormSubmit(evt) {
     closePopup(popupEdit);
 };
 
-// Кнопка сабмита формы редактирования
+// Cабмит формы редактирования
 formEdit.addEventListener('submit', editFormSubmit);
 
 // Функция создания, удаления и просмотра карточек
@@ -76,13 +78,11 @@ function createCard(titleValue, photoValue) {
   cardPhoto.src = photoValue;
   cardPhoto.alt = titleValue;
 
-   // Просмотр карточки 
-  cardPhoto.addEventListener('click', () => { 
-    document.querySelector('.popup-open-picture__image').src = photoValue;
-    document.querySelector('.popup-open-picture__image').alt = titleValue;
-    document.querySelector('.popup-open-picture__caption').textContent = titleValue;
-    openPopup(popupPicture);
-  });
+  popupHandler(cardPhoto, popupPicture, () => {
+    popupPicture.querySelector('.popup-picture__image').src = photoValue;
+    popupPicture.querySelector('.popup-picture__image').alt = titleValue;
+    popupPicture.querySelector('.popup-picture__caption').textContent = titleValue;
+  })
 
   // Смена цвета кнопки лайка
   cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => {
@@ -108,15 +108,12 @@ function addFormSubmit(evt) {
   const card = createCard(pictureNameInput.value, linkInput.value);
 
   addCard(card, gallery);
-
+  evt.target.querySelector('.form__button').classList.add('form__button_disabled')
   closePopup(popupAdd);
 };
 
 // Кнопка сабмита формы добавления карточек
 formAdd.addEventListener('submit', addFormSubmit);
-
-//Функция закрытия попапа с изображением
-popupPictureClose.addEventListener('click', () => closePopup(popupPicture));
 
 // Функция заполнения галереи карточками из массива
 function addCardsArray (array) {
@@ -130,9 +127,71 @@ function addCardsArray (array) {
 // Вызов функции заполнения галереи 
 addCardsArray(initialCards);
 
+// Валидация форм
+const showInputError = (form, input, errorMessage) => {
+  const error = form.querySelector(`.${input.id}-error`);
+  input.classList.add('form__input_type_error');
+  error.textContent = errorMessage;
+  error.classList.add('form__error_active')
+}
 
+const hideInputError = (form, input) => {
+  const error = form.querySelector(`.${input.id}-error`);
+  input.classList.remove('form__input_type_error');
+  input.classList.remove('form__input_type_error');
+  error.textContent = '';
+}
 
+const checkValidity = (form, input) =>{
+  if (!input.validity.valid) {
+    showInputError(form, input, input.validationMessage)
+  } else {
+    hideInputError(form, input)
+  }
+}
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some(input => {
+    return !input.validity.valid;
+  })
+}
 
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add('form__button_disabled');
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove('form__button_disabled')
+  }
+}
 
+const setEventListeners = (form) => {
+  const inputList = Array.from(form.querySelectorAll('.form__input'));
+  const buttonSubmit = form.querySelector('.form__button');
+  toggleButtonState(inputList, buttonSubmit)
+  
+  inputList.forEach(input => {
+    input.addEventListener('input', () => {
+      checkValidity(form, input);
+      toggleButtonState(inputList, buttonSubmit)
+    })
+  })
+}
 
+const enableValidation = () => {
+  Array.from(document.forms).forEach(form => {
+    setEventListeners(form);
+  })
+}
+
+enableValidation()
+
+// ПРойти регулярки еще раз...................
+
+let regEx = /\W+\-/gi;
+
+let a = 'бибаc - боба'
+
+console.log(regEx.test(a));
+console.log(a.match(regEx));
