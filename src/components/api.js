a1ac6047ca5db6395db45fef6a09f60ad05f9217
Boris <savelyevbb@mail.ru>
2023-03-20 18:32:15 +0500

    refactoring api

--- src/components/api.js
+++ src/components/api.js
@@ -1,90 +1,92 @@
-import { checkResponse } from "./utils.js";
+class Api {
+  constructor({baseUrl, headers}) {
+    this.url = baseUrl;
+    this.headers = headers;
+  }
 
-// Конфигурация для запросов
-export const requestConfig = {
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка ${res.status}`)
+  }
+
+  _request(endpoint, options) {
+    return fetch(`${this.url}${endpoint}`, options).then(this._checkResponse);
+  }
+
+  setProfileAbout(nameInput, aboutInput) {
+    return this._request('/users/me', {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: nameInput,
+        about: aboutInput
+      })
+    })
+  }
+
+  getProfileAbout() {
+    return this._request('/users/me', {
+      method: 'GET',
+      headers: this.headers
+    })
+  }
+
+  getInitialCards() {
+    return this._request('/cards', {
+      method: 'GET',
+      headers: this.headers
+    })
+  }
+
+  addCardRequest(nameInput, linkInput) {
+    return this._request('/cards', {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: nameInput,
+        link: linkInput
+      })
+    })
+  }
+
+  setLikeRequest(cardId) {
+    return this._request(`/cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this.headers
+    })
+  }
+
+  removeLikeRequest(cardId) {
+    return this._request(`/cards/likes/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers
+    })
+  }
+
+  removeCard(cardId) {
+    return this._request(`/cards/${cardId}`,  {
+      method: 'DELETE',
+      headers: this.headers
+    })
+  }
+
+  changeAvatar(linkInput) {
+    return this._request('/users/me/avatar', {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        avatar: linkInput
+      })
+    });
+  }
+}
+
+export const api = new Api({
   baseUrl: 'https://nomoreparties.co/v1/plus-cohort-20',
   headers: {
     authorization: 'c29acbcd-e400-4694-90fd-37fde0fe5a19',
     'Content-Type': 'application/json'
   }
-};
-
-// Функция запроса
-const request = (endpoint, options) => {
-  return fetch(`${requestConfig.baseUrl}${endpoint}`, options).then(checkResponse);
-}
-
-// Запрос на изменение имени и профессии
-export const setProfileAbout = (nameInput, aboutInput) => {
- return request('/users/me', {
-    method: 'PATCH',
-    headers: requestConfig.headers,
-    body: JSON.stringify({
-      name: nameInput,
-      about: aboutInput
-   })
-  })
-};
-
-// Запрос на получение данных профиля 
-export const getProfileAbout = () => {
-  return request('/users/me', {
-    method: 'GET',
-    headers: requestConfig.headers
-  })
-};
-
-// Запрос на заполнение галереи карточками
-export const getProfileCards = () => {
-  return request('/cards', {
-    method: 'GET',
-    headers: requestConfig.headers
-  })
-};
-
-// Запрос на добалвение карточки
-export const addCardRequest = (nameInput, linkInput) => {
-  return request('/cards', {
-    method: 'POST',
-    headers: requestConfig.headers,
-    body: JSON.stringify({
-      name: nameInput,
-      link: linkInput
-    })
-  })
-};
-
-// Поставить лайк
-export const setLikeRequest = (cardId) => {
-  return request(`/cards/likes/${cardId}`, {
-    method: 'PUT',
-    headers: requestConfig.headers
-  })
-};
-
-// Снять лайк
-export const removeLikeRequest = (cardId) => {
-  return request(`/cards/likes/${cardId}`, {
-    method: 'DELETE',
-    headers: requestConfig.headers
-  })
-};
-
-// Удаление карточки
-export const removeCard = (cardId) => {
-  return request(`/cards/${cardId}`,  {
-    method: 'DELETE',
-    headers: requestConfig.headers
-  })
-};
-
-// Обновление аватара
-export const changeAvatar = (linkInput) => {
-  return request('/users/me/avatar', {
-    method: 'PATCH',
-    headers: requestConfig.headers,
-    body: JSON.stringify({
-      avatar: linkInput
-    })
-  });
-}
+})
